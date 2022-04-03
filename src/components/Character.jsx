a988b97c859406c4f040778c430d50f111d061f9
Andrés Fernández <lafvg12@hotmail.com>
2022-04-02 20:23:05 -0500

    continuos integration with github actions3

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Grid from "@material-ui/core/Grid";
-import { CardMarvel } from "./CardMarvel";
+// import { CardMarvel } from "./CardMarvel";
 import styled from "styled-components";
 
 export const Div = styled.div`
@@ -14,16 +14,16 @@ export const Div2 = styled.div`
 `;
 
 const Character = () => {
-  const URL = process.env.REACT_APP_API_KEY_MARVEL;
-  const [character, setCharacter] = useState([]);
+  // const URL = process.env.REACT_APP_API_KEY_MARVEL;
+  // const [character, setCharacter] = useState([]);
 
-  const getCharacter = () => {
-    fetch(`${URL}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setCharacter(data.data.results);
-      });
-  };
+  // const getCharacter = () => {
+  //   fetch(`${URL}`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setCharacter(data.data.results);
+  //     });
+  // };
 
   // useEffect(() => {
   //   getCharacter();
@@ -31,13 +31,13 @@ const Character = () => {
 
   return (
     <Div>
-      <Div2>
+      {/* <Div2>
         <Grid container spacing={2}>
           {character.map((characters) => (
             <CardMarvel key={character.id} data={characters} />
           ))}
         </Grid>
-      </Div2>
+      </Div2> */}
     </Div>
   );
 };
